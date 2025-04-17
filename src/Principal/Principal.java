ad5df633670065a0b6d8993fbcf2085df95ea5c7
ArcanjoPaquissi <arcanjopaquissi@gmail.com>
2025-04-17 10:45:24 -0300

    Implementei a estrutura condiconal

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -3,8 +3,13 @@ package Principal;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.err.println("Olá Arcanjo!! Você está na aula de Banco de Dados");
+		
+		int numero = 10;
+		if (numero > 5) {
+			System.out.println("numero > 5");
+		}
 	}
 
 }
